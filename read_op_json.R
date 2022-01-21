2c6d057ec220b14077648e16749de47c05c93274
John Franchak <franchak@gmail.com>
2022-01-21 10:28:49 -0800

    keypoint labels

--- read_op_json.R
+++ read_op_json.R
@@ -12,7 +12,7 @@ extract_keypoints <- function(file_name) {
   frame <- fromJSON(file_name)
   file_num <- as.numeric(str_extract(file_name,"\\d\\d\\d\\d\\d\\d\\d\\d\\d\\d\\d\\d"))
   labs <- rep(c("xloc","yloc","conf"),25)
-  keypoints <- ceiling(seq(1:75)/3)
+  keypoints <- floor(seq(1:75)/3)
   if (length(frame$people$pose_keypoints_2d) > 0) {
     first_loop = TRUE
     for (i in 1:length(frame$people$pose_keypoints_2d)) {
@@ -27,7 +27,7 @@ extract_keypoints <- function(file_name) {
       }
     }
   } else {
-    output <- tibble(file_num = file_num, person = NA, keypoint = NA, xloc = NA, yloc = NA, conf = NA)
+    output <- tibble(file_num = file_num, person = NA, keypoints = NA, xloc = NA, yloc = NA, conf = NA)
   }
   return(output)
 }
@@ -60,7 +60,7 @@ keypoint_lab <- c("Nose",
 "LHeel",
 "RBigToe",
 "RSmallToe",
-"RHeel",
-"Background")
+"RHeel")
 
+walk <- walk %>% mutate(kp = factor(keypoints, levels = 0:24, labels = keypoint_lab))
 
